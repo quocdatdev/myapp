509c847d34c4920ce662da2c92ee67298c560a56
Cr4zzee <tungtran.uneti@gmail.com>
2020-06-03 22:59:42 +0700

    update logs

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 const express = require('express');
-const POST = process.env.POST || 3000;
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const app = express();
-
+const PORT = process.env.PORT || 3000;
+app.use(morgan(':date[iso] :method :url :status :res[content-length] - :response-time ms | :req[header] | :req[query] | :req[body]'))
+app.use(morgan(':date[iso] :method :url :status :res[content-length] - :response-time ms | :req[header] | :req[query] | :req[body]',{
+    stream: fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' })
+  }))
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/index', routes);
-
-app.listen(POST, console.log('Server is running'));
+app.use('/',(req,res)=>{
+    res.send('Your server still alive!')
+})
+app.listen(PORT, console.log('Server is running on port',PORT));
 
