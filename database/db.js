445d0b58f509c4ed2d5b50f3fc59a56a1f50b596
QuocDatDev <quocdatsh93@gmail.com>
2020-06-04 09:41:30 +0700

    update app.js & db.js

--- database/db.js
+++ database/db.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-let connect = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
-});
+// const connect = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
 
-connect.connect((err) => {
-	if(err) {
-		console.log('Error connecting to db!!!');
-	}
-	console.log('Connection establish!!!');
-});
+// connect.connect((err) => {
+//   if (err) {
+//     console.log('Error connecting to db!!!');
+//   }
+//   console.log('Connection establish!!!');
+// });
 
-module.exports = connect;
+// module.exports = connect;
